52fb9c87a6da204616367a9f8e13aa475ba3449e
李子木 <zimu.li@renren-inc.com>
2023-03-28 21:52:53 +0800

    equals and hashcode call super

--- lucky-persistence/src/main/java/pers/zymir/lucky/po/LuckyAward.java
+++ lucky-persistence/src/main/java/pers/zymir/lucky/po/LuckyAward.java
@@ -1,7 +1,9 @@
 package pers.zymir.lucky.po;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class LuckyAward extends BaseEntity{
   private Long awardId;
